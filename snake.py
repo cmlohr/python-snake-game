f294406134bfcc3c4d44d6c704196e1b822dfcfd
Colleen <cmlohr@outlook.com>
2020-11-21 11:54:30 -0600

    game init

--- snake.py
+++ snake.py
@@ -1,3 +1,50 @@
 from turtle import Turtle, Screen
-import random
 
+screen = Screen()
+# constants
+STARTING_POSITIONS = [(-40, 0), (0, 0), (-20, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
+
+
+
+class Snake:
+
+    def __init__(self):
+        self.segment = []
+        self.create_snake()
+        self.head = self.segment[0]
+
+    def create_snake(self):
+        for snake_body in STARTING_POSITIONS:
+            snake = Turtle(shape="square")
+            snake.color("white")
+            snake.penup()
+            snake.goto(snake_body)
+            self.segment.append(snake)
+
+    def move(self):
+        for seg_num in range(len(self.segment) - 1, 0, -1):
+            new_x = self.segment[seg_num - 1].xcor()
+            new_y = self.segment[seg_num - 1].ycor()
+            self.segment[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
